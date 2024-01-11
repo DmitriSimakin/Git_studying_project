4dc327ded18de46edd9f1e7314a1b7b4547409bb
Dmitri_Simakin <118720205+DmitriiSimakin@users.noreply.github.com>
2024-01-11 03:10:32 +0300

    add some features

--- logic.py
+++ logic.py
@@ -1,3 +1,6 @@
 import numpy as np
 import math
 import time
+
+def func_1(x, y):
+	return x, y
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import numpy as np
 import math
 import time
+
+if __name__ == "__main__":
+	print("Hello!")
